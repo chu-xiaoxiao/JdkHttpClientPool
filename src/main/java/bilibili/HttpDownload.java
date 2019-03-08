6f0de733adbe4954f574ce19aa267bacf105fd3b
zyc <zhangyc@missfresh.cn>
2019-03-08 13:47:59 +0800

    修改下载类

--- src/main/java/bilibili/HttpDownload.java
+++ src/main/java/bilibili/HttpDownload.java
@@ -2,22 +2,12 @@ package bilibili;
 
 import http.HttpRequest;
 import lombok.extern.slf4j.Slf4j;
-import old.util.HttpClientUtils;
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.springframework.util.CollectionUtils;
 import thread.DownloadThreadFactory;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
 import java.math.BigDecimal;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.text.NumberFormat;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -43,38 +33,15 @@ import java.util.stream.Stream;
 public class HttpDownload {
 
 
-    private static final Queue<DownloadPath> DOWNLOADPATHS = new LinkedBlockingDeque<>();
-
-    private static final int cache = 10 * 1024;
-    private static final boolean isWindows;
-    private static final String splash;
-    private static final String root;
-    private static final HttpClientUtils HTTP_CLIENT_UTILS = new HttpClientUtils();
+    private static final Queue<DownloadPath> DOWNLOAD_PATHS = new LinkedBlockingDeque<>();
 
     private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(6, 10, 10, TimeUnit.MINUTES, new LinkedBlockingDeque<>(),new DownloadThreadFactory("下载线程"));
 
     static {
-        if (System.getProperty("os.name") != null && System.getProperty("os.name").toLowerCase().contains("windows")) {
-            isWindows = true;
-            splash = "\\";
-            root = "D:";
-        } else {
-            isWindows = false;
-            splash = "/";
-            root = "/search";
-        }
+        //初始化下载线程
         Stream.iterate(0, item -> ++item).limit(5).forEach(item -> threadPoolExecutor.execute(new DownloadThread()));
     }
 
-    /**
-     * 根据url下载文件，文件名从response header头中获取
-     *
-     * @param url
-     * @return
-     */
-    public String download(String url) {
-        return download(url, null);
-    }
 
     /**
      * 根据url下载文件，保存到filepath中
@@ -83,28 +50,22 @@ public class HttpDownload {
      * @param filepath
      * @return
      */
-    public static String download(String url, String filepath) {
-        DOWNLOADPATHS.add(new DownloadPath(url, filepath));
-        //threadPoolExecutor.execute();
-        return "";
+    public static void download(String url, String filepath) {
+        DOWNLOAD_PATHS.add(new DownloadPath(url, filepath));
     }
 
     static class DownloadThread implements Runnable {
 
         volatile Boolean done = true;
 
-        public DownloadThread() {
-
-        }
-
         @Override
         public void run() {
             for (; ; ) {
                 try {
-                    while (!this.done || CollectionUtils.isEmpty(DOWNLOADPATHS)) {
+                    while (!this.done || CollectionUtils.isEmpty(DOWNLOAD_PATHS)) {
                         Thread.sleep(100);
                     }
-                    DownloadPath downloadPath = DOWNLOADPATHS.remove();
+                    DownloadPath downloadPath = DOWNLOAD_PATHS.remove();
                     this.done = false;
                     Path savePath = Path.of(downloadPath.path);
                     Files.createDirectories(savePath.getParent());
@@ -122,67 +83,6 @@ public class HttpDownload {
                     e.printStackTrace();
                 }
             }
-        }
-    }
-
-    /**
-     * 获取response要下载的文件的默认路径
-     *
-     * @param response
-     * @return
-     */
-    public static String getFilePath(HttpResponse response) {
-        String filepath = root + splash;
-        String filename = getFileName(response);
-
-        if (filename != null) {
-            filepath += filename;
-        } else {
-            filepath += getRandomFileName();
-        }
-        return filepath;
-    }
-
-    /**
-     * 获取response header中Content-Disposition中的filename值
-     *
-     * @param response
-     * @return
-     */
-    public static String getFileName(HttpResponse response) {
-        Header contentHeader = response.getFirstHeader("Content-Disposition");
-        String filename = null;
-        if (contentHeader != null) {
-            HeaderElement[] values = contentHeader.getElements();
-            if (values.length == 1) {
-                NameValuePair param = values[0].getParameterByName("filename");
-                if (param != null) {
-                    try {
-                        //filename = new String(param.getValue().toString().getBytes(), "utf-8");  
-                        //filename=URLDecoder.decode(param.getValue(),"utf-8");  
-                        filename = param.getValue();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        return filename;
-    }
-
-    /**
-     * 获取随机文件名
-     *
-     * @return
-     */
-    public static String getRandomFileName() {
-        return String.valueOf(System.currentTimeMillis());
-    }
-
-    public static void outHeaders(HttpResponse response) {
-        Header[] headers = response.getAllHeaders();
-        for (int i = 0; i < headers.length; i++) {
-            System.out.println(headers[i]);
         }
     }
 
